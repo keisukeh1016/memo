5ed6d28992b197ac7ed1977733b37642a768b7ef
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2021-01-03 19:44:07 +0900

    プロジェクト ファイルを追加します。

---
+++ Memo/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstTitle = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lstTitle
+        '
+        Me.lstTitle.FormattingEnabled = True
+        Me.lstTitle.ItemHeight = 15
+        Me.lstTitle.Location = New System.Drawing.Point(87, 86)
+        Me.lstTitle.Name = "lstTitle"
+        Me.lstTitle.Size = New System.Drawing.Size(242, 424)
+        Me.lstTitle.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1286, 659)
+        Me.Controls.Add(Me.lstTitle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstTitle As ListBox
+End Class
---
+++ Memo/Form1.vb
@@ -0,0 +1,20 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Form1
+    'Private sConnect As String = "Data Source=INSPIRON5505;Initial Catalog=memo;Integrated Security=True"
+
+    Private sTitle() As String = {"aaa", "bbb", "ccc"}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        For Each item As String In sTitle
+            lstTitle.Items.Add(item)
+        Next
+    End Sub
+
+End Class
+
+Public Class Memo
+
+
+End Class
