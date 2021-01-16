bede1a5de663a9da56a21d244d1a297172db962c
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2021-01-17 03:08:30 +0900

    show memo title and detail

--- Memo/Form1.Designer.vb
+++ Memo/Form1.Designer.vb
@@ -22,29 +22,101 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.lstTitle = New System.Windows.Forms.ListBox()
-        Me.SuspendLayout()
+        Me.lstMemoTitle = New System.Windows.Forms.ListBox()
+        Me.txtMemoDetail = New System.Windows.Forms.TextBox()
+        Me.txtMemoTitle = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout
+        Me.SuspendLayout
         '
-        'lstTitle
+        'lstMemoTitle
         '
-        Me.lstTitle.FormattingEnabled = True
-        Me.lstTitle.ItemHeight = 15
-        Me.lstTitle.Location = New System.Drawing.Point(87, 86)
-        Me.lstTitle.Name = "lstTitle"
-        Me.lstTitle.Size = New System.Drawing.Size(242, 424)
-        Me.lstTitle.TabIndex = 0
+        Me.lstMemoTitle.BackColor = System.Drawing.Color.Silver
+        Me.lstMemoTitle.Font = New System.Drawing.Font("MS UI Gothic", 24!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128,Byte))
+        Me.lstMemoTitle.FormattingEnabled = true
+        Me.lstMemoTitle.ItemHeight = 40
+        Me.lstMemoTitle.Location = New System.Drawing.Point(0, 50)
+        Me.lstMemoTitle.Name = "lstMemoTitle"
+        Me.lstMemoTitle.Size = New System.Drawing.Size(300, 644)
+        Me.lstMemoTitle.TabIndex = 0
+        '
+        'txtMemoDetail
+        '
+        Me.txtMemoDetail.BackColor = System.Drawing.Color.LightGray
+        Me.txtMemoDetail.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtMemoDetail.Font = New System.Drawing.Font("MS UI Gothic", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128,Byte))
+        Me.txtMemoDetail.Location = New System.Drawing.Point(9, 90)
+        Me.txtMemoDetail.Multiline = true
+        Me.txtMemoDetail.Name = "txtMemoDetail"
+        Me.txtMemoDetail.Size = New System.Drawing.Size(900, 400)
+        Me.txtMemoDetail.TabIndex = 2
+        Me.txtMemoDetail.Text = "内容"
+        '
+        'txtMemoTitle
+        '
+        Me.txtMemoTitle.BackColor = System.Drawing.Color.LightGray
+        Me.txtMemoTitle.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtMemoTitle.Font = New System.Drawing.Font("MS UI Gothic", 36!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128,Byte))
+        Me.txtMemoTitle.Location = New System.Drawing.Point(9, 10)
+        Me.txtMemoTitle.Name = "txtMemoTitle"
+        Me.txtMemoTitle.Size = New System.Drawing.Size(900, 60)
+        Me.txtMemoTitle.TabIndex = 3
+        Me.txtMemoTitle.Text = "タイトル"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.btnSave)
+        Me.Panel1.Controls.Add(Me.txtMemoTitle)
+        Me.Panel1.Controls.Add(Me.txtMemoDetail)
+        Me.Panel1.Location = New System.Drawing.Point(320, 20)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(920, 600)
+        Me.Panel1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128,Byte))
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(300, 50)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "メモリスト"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("MS UI Gothic", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128,Byte))
+        Me.btnSave.Location = New System.Drawing.Point(399, 520)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(120, 50)
+        Me.btnSave.TabIndex = 4
+        Me.btnSave.Text = "保存"
+        Me.btnSave.UseVisualStyleBackColor = true
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8!, 15!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1286, 659)
-        Me.Controls.Add(Me.lstTitle)
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(1262, 673)
+        Me.Controls.Add(Me.lstMemoTitle)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        Me.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(false)
+        Me.Panel1.PerformLayout
+        Me.ResumeLayout(false)
 
-    End Sub
+End Sub
 
-    Friend WithEvents lstTitle As ListBox
+    Friend WithEvents lstMemoTitle As ListBox
+    Friend WithEvents txtMemoDetail As TextBox
+    Friend WithEvents txtMemoTitle As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSave As Button
 End Class
--- Memo/Form1.vb
+++ Memo/Form1.vb
@@ -1,20 +1,68 @@
 ﻿Option Explicit On
 Option Strict On
 
-Public Class Form1
-    'Private sConnect As String = "Data Source=INSPIRON5505;Initial Catalog=memo;Integrated Security=True"
+Imports System.Data.SqlClient
 
-    Private sTitle() As String = {"aaa", "bbb", "ccc"}
+Public Class Form1
+    Private memoData As New MemoData
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
-        For Each item As String In sTitle
-            lstTitle.Items.Add(item)
-        Next
+        memoData.Read()
+
+        lstMemoTitle.DataSource = memoData.memos
+        lstMemoTitle.DisplayMember = "Title"
+
+        ClearListSelection()
     End Sub
 
+    Private Sub showMemo(memo As Memo)
+        txtMemoTitle.Text = memo.Title
+        txtMemoDetail.Text = memo.Detail
+    End Sub
+
+    Public Sub ClearListSelection()
+        lstMemoTitle.SelectedIndex = -1
+
+        txtMemoTitle.Clear()
+        txtMemoDetail.Clear()
+    End Sub
+
+    'イベント処理
+    Private Sub lstMemoTitle_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstMemoTitle.SelectedIndexChanged
+        If lstMemoTitle.SelectedIndex = -1 Then
+            ClearListSelection()
+        Else
+            showMemo( memoData.memos(lstMemoTitle.SelectedIndex) )
+        End If
+    End Sub
 End Class
 
+
 Public Class Memo
+    Public Property Id As Integer
+    Public Property Title As String
+    Public Property Detail As String
 
+    Public Sub New(datarow As DataRow)
+        Id     = CInt( datarow("id") )
+        Title  = CStr( datarow("title") )
+        Detail = CStr( datarow("detail") )
+    End Sub
+End Class
 
+Public Class MemoData
+    Public connection As String = "Data Source=INSPIRON5505;Initial Catalog=memo;Integrated Security=True"
+    Public query As String = "SELECT * FROM memo.dbo.memo ORDER BY memo.dbo.memo.id asc"
+    Public adapter As New SqlDataAdapter(query, connection)
+    Public dataset As New DataSet
+    Public memos As Memo()
+
+    Public Sub Read() 
+        adapter.Fill(dataset, "memo")
+        ReDim memos(dataset.Tables("memo").Rows.Count - 1)
+
+        For index = 0 To dataset.Tables("memo").Rows.Count - 1
+            memos(index) = New Memo(dataset.Tables("memo").Rows(index))
+        Next
+    End Sub
 End Class
